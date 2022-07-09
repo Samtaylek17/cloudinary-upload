836b4fb3488406f7f90266f29b1c9d45baf0804d
samtaylek17 <ogunsusitemitayo99@gmail.com>
2022-07-09 10:42:38 +0100

    Form submission with FormSubmit.co

--- js/app.js
+++ js/app.js
@@ -1,23 +1,41 @@
-// Add Cloud name to url
-const url = 'https://api.cloudinary.com/v1_1/<your-cloud-name>/auto/upload';
-const form = document.querySelector('form');
+const url = 'https://api.cloudinary.com/v1_1/samtaylek/auto/upload';
+const form = document.querySelector('#fileUpload');
+// console.log(form);
 
-form.addEventListener('submit', async (e) => {
-	e.preventDefault();
+form.onchange = async (e) => {
+	const files = e.target.files;
+	console.log(files);
 
-	const files = document.querySelector('[type=file]').files;
 	const formData = new FormData();
 
 	for (let i = 0; i < files.length; i++) {
 		let file = files[i];
 		formData.append('file', file);
-		// Add your Upload preset here
-		formData.append('upload_preset', 'your-upload-preset');
+		formData.append('upload_preset', 'cxqd6xsg');
 
 		const response = await axios.post(url, formData, { headers: { 'Content-Type': 'multipart/form-data' } });
-
-		console.log(response.data.url);
-
-		document.getElementById('viewSubmission').setAttribute('href', response.data.url);
+		document.getElementById('fileUrl').setAttribute('value', response.data.url);
 	}
-});
+};
+
+// form.addEventListener('onchange', async (e) => {
+// 	// e.preventDefault();
+// 	console.log('I am working');
+
+// 	console.log(e.target.files);
+
+// 	const files = document.querySelector('[type=file]').files;
+// 	const formData = new FormData();
+
+// 	// for (let i = 0; i < files.length; i++) {
+// 	// 	let file = files[i];
+// 	// 	formData.append('file', file);
+// 	// 	formData.append('upload_preset', 'cxqd6xsg');
+
+// 	// 	const response = await axios.post(url, formData, { headers: { 'Content-Type': 'multipart/form-data' } });
+
+// 	// 	console.log(response.data.url);
+
+// 	// 	document.getElementById('viewSubmission').setAttribute('href', response.data.url);
+// 	// }
+// });
